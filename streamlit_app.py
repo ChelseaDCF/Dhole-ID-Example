2227ae6a3412f306f1bb8f40230bba687201d571
ChelseaDCF <122062141+ChelseaDCF@users.noreply.github.com>
2023-07-11 10:08:15 -0700

    Update streamlit_app.py
    Change file from previous version to new pkl file

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def load_model():
     plt = platform.system()
 
     if plt == 'Linux' or plt == 'Darwin': pathlib.WindowsPath = pathlib.PosixPath
-    inf_model = load_learner('perumixed3.pkl', cpu=True)
+    inf_model = load_learner('Dhole ID.pkl', cpu=True)
 
     return inf_model
 
